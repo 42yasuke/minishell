799ac0224cd71a9c3352ad8d7e22458d43ea2937
42yasuke <j.ralph1998@gmail.com>
2023-05-20 17:34:09 +0200

    on reconnait les buitins

--- src/builtin.c
+++ src/builtin.c
@@ -6,7 +6,7 @@
 /*   By: jose <jose@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/20 11:38:16 by jose              #+#    #+#             */
-/*   Updated: 2023/05/20 17:16:40 by jose             ###   ########.fr       */
+/*   Updated: 2023/05/20 17:31:08 by jose             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,19 @@
 
 int	ft_is_builtin(char *line)
 {
-	int	i;
-
-	(void)i;
-	(void)line;
-	return (true);
+	if (!ft_strncmp(line, "cd", 2))
+		return (true);
+	if (!ft_strncmp(line, "echo", 4))
+		return (true);
+	if (!ft_strncmp(line, "pwd", 3))
+		return (true);
+	if (!ft_strncmp(line, "export", 6))
+		return (true);
+	if (!ft_strncmp(line, "unset", 5))
+		return (true);
+	if (!ft_strncmp(line, "env", 3))
+		return (true);
+	if (!ft_strncmp(line, "exit", 4))
+		return (true);
+	return (false);
 }
