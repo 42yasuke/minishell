c4ad1e35a61b7aacc0c36e985c913228fdd58069
42yasuke <j.ralph1998@gmail.com>
2023-05-31 19:20:45 +0200

    on a les redir

--- src/parse.c
+++ src/parse.c
@@ -6,7 +6,7 @@
 /*   By: jose <jose@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/21 11:54:37 by jose              #+#    #+#             */
-/*   Updated: 2023/05/28 21:44:16 by jose             ###   ########.fr       */
+/*   Updated: 2023/05/31 19:18:58 by jose             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,51 +46,65 @@ t_cmd	*ft_parsepipe(char **ps, char **envp)
 	return (cmd);
 }
 
-t_cmd	*ft_parseredir(t_cmd *cmd, char **ps)
+/*
+char	*file
+char	*start_red
+char	*end_red*/
+
+t_cmd	*ft_parseredir(t_cmd *cmd, char **ps) //maj
 {
 	char	*file;
-	char	*tmp;
+	char	*infile;
+	char	*outfile;
+	//char	*tmp;
 
-	tmp = ft_strchr(*ps, '<');
-	if (tmp && g_inf->interpret)
+	infile = ft_strchr(*ps, '<');
+	outfile = ft_strchr(*ps, '>');
+	if (infile && g_inf->interpret)
 	{
-		tmp++;
-		ft_peek(&tmp);
-		file = tmp;
-		while (*tmp && !ft_is_whitespace(*tmp))
-			tmp++;
-		if (*tmp)
+		infile++;
+		ft_peek(&infile);
+		file = infile;
+		while (*infile && !ft_is_whitespace(*infile))
+			infile++;
+		if (*infile)
 		{
-			*tmp = '\0';
-			tmp++;
+			*infile = '\0';
+			infile++;
 		}
-		*ps = tmp;
+		if (**ps == '<') //test
+			*ps = infile;
+		else
+		{
+			infile = ft_strchr(*ps, '<');
+			infile--;
+			*infile = '\0';
+		}
 		cmd = ft_redircmd(cmd, file, O_RDONLY, STDIN_FILENO);
 	}
-	tmp = ft_strchr(*ps, '>');
-	if (tmp && g_inf->interpret)
+	if (outfile && g_inf->interpret)
 	{
-		*tmp = '\0';
-		tmp++;
-		if (*tmp == '>')
+		*outfile = '\0';
+		outfile++;
+		if (*outfile == '>')
 		{
-			tmp++;
-			ft_peek(&tmp);
-			file = tmp;
-			while (*tmp && !ft_is_whitespace(*tmp))
-				tmp++;
-			if (*tmp)
-				*tmp = '\0';
+			outfile++;
+			ft_peek(&outfile);
+			file = outfile;
+			while (*outfile && !ft_is_whitespace(*outfile))
+				outfile++;
+			if (*outfile)
+				*outfile = '\0';
 			cmd = ft_redircmd(cmd, file, O_WRONLY | O_CREAT | O_APPEND, STDOUT_FILENO);
 		}
 		else
 		{
-			ft_peek(&tmp);
-			file = tmp;
-			while (*tmp && !ft_is_whitespace(*tmp))
-				tmp++;
-			if (*tmp)
-				*tmp = '\0';
+			ft_peek(&outfile);
+			file = outfile;
+			while (*outfile && !ft_is_whitespace(*outfile))
+				outfile++;
+			if (*outfile)
+				*outfile = '\0';
 			cmd = ft_redircmd(cmd, file, O_WRONLY | O_CREAT | O_TRUNC, STDOUT_FILENO);
 		}
 	}
@@ -104,6 +118,7 @@ t_cmd	*ft_parseexec(char **ps, char **envp)
 
 	ret = ft_execcmd(envp);
 	ecmd = (t_ecmd*)ret;
+	//ft_peek(ps);
 	ret = ft_parseredir(ret, ps);
 	ecmd->argv = ft_split(*ps, ' ');
 	ft_getpath_n_builtin(ecmd);
