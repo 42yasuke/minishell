203e1c670d4a0c5a6597a133d9e1a2b250ce5e09
Jose RALPH <jralph@made-f0Br7s6.clusters.42paris.fr>
2023-10-20 14:18:37 +0200

    /ls fixed

--- src/path.c
+++ src/path.c
@@ -6,38 +6,60 @@
 /*   By: jralph <jralph@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/21 20:23:18 by jose              #+#    #+#             */
-/*   Updated: 2023/10/19 13:38:29 by jralph           ###   ########.fr       */
+/*   Updated: 2023/10/20 13:55:39 by jralph           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini.h"
 
-static char	*ft_get_path(char *cmd, char **env)
+static int	ft_verif_strcmd(char *cmd)
 {
-	char	*path_envp;
-	char	**mypaths;
-	int		i;
-	char	*ret;
-	char	*cmd_to_test;
+	int	i;
 
-	path_envp = ft_getenv("PATH", env);
-	if (!path_envp)
-		return (cmd);
-	mypaths = ft_split(path_envp, ':');
 	i = -1;
-	ret = cmd;
-	if (ret && (!access(ret, X_OK) || (*ret == '.' && *(ret + 1) == '/')))
-		return (ft_free_all(mypaths), ret);
+	while (cmd && cmd[++i])
+	{
+		if (cmd[i] == '.' && cmd[i + 1] == '/')
+			return (true);
+		if (cmd[i] == '/' && cmd[i + 1] == '/')
+			return (true);
+	}
+	return (false);
+}
+
+static char	*ft_loop(char *path_envp, char *cmd)
+{
+	int		i;
+	char	**mypaths;
+	char	*cmd_to_test;
+	char	*ret;
+
+	i = -1;
 	cmd_to_test = ft_strjoin("/", cmd);
+	mypaths = ft_split(path_envp, ':');
 	while (mypaths[++i])
 	{
 		ret = ft_strjoin(mypaths[i], cmd_to_test);
 		if (!access(ret, X_OK))
 			break ;
 		free(ret);
-		ret = cmd;
+		ret = NULL;
 	}
 	return (free(cmd_to_test), ft_free_all(mypaths), ret);
+}
+
+static char	*ft_get_path(char *cmd, char **env)
+{
+	char	*path_envp;
+	char	*ret;
+
+	path_envp = ft_getenv("PATH", env);
+	if (!path_envp || (cmd && (!access(cmd, X_OK))))
+		return (cmd);
+	ret = ft_loop(path_envp, cmd);
+	if (ret && ft_verif_strcmd(ret))
+		(free(ret), ret = cmd);
+	return (ret);
 }
 
 void	ft_getpath_n_builtin(t_ecmd *ecmd)
