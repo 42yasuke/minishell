73d96d2cc5e21660fc9fe13c1f0a85ac708796e0
Jose RALPH <jralph@made-f0Br7s6.clusters.42paris.fr>
2023-10-23 15:57:05 +0200

    everything is fixed

--- src/ft_update_env.c
+++ src/ft_update_env.c
@@ -6,31 +6,39 @@
 /*   By: jralph <jralph@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/10 15:48:08 by jose              #+#    #+#             */
-/*   Updated: 2023/10/16 16:44:10 by jralph           ###   ########.fr       */
+/*   Updated: 2023/10/23 15:29:03 by jralph           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mini.h"
 
-static void	ft_find_n_update_env_name_in_env(char **env, char *str, int lst)
+static void	ft_help(char **env, char *str, int lst, int i)
 {
 	char	*tmp;
+
+	tmp = env[i];
+	if (lst && ft_strchr(str, '='))
+		env[i] = ft_add_guigui_on_env_name(str);
+	else if (!lst)
+		env[i] = ft_strdup(str);
+	if ((lst && ft_strchr(str, '=')) || !lst)
+		free(tmp);
+}
+
+static void	ft_find_n_update_env_name_in_env(char **env, char *str, int lst)
+{
 	int		i;
 	size_t	diff;
+	size_t	diff2;
 
 	i = -1;
+	diff2 = ft_strlen(str) - ft_strlen(ft_strchr(str, '='));
 	while (env[++i])
 	{
 		diff = ft_strlen(env[i]) - ft_strlen(ft_strchr(env[i], '='));
-		if (!ft_strncmp(str, env[i], diff))
+		if (!ft_strncmp(str, env[i], diff) && diff == diff2)
 		{
-			tmp = env[i];
-			if (lst && ft_strchr(str, '='))
-				env[i] = ft_add_guigui_on_env_name(str);
-			else if (!lst)
-				env[i] = ft_strdup(str);
-			if ((lst && ft_strchr(str, '=')) || !lst)
-				free(tmp);
+			ft_help(env, str, lst, i);
 			break ;
 		}
 	}
